05fcc33118a700e4f0b8ada6ac9a33a6d1edec11
James Cool <jeffreysbrother@gmail.com>
2016-10-11 10:42:27 -0700

    test todos variable

--- scripts/app.js
+++ scripts/app.js
@@ -5,18 +5,14 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("this is the helloWorld controller function in the mainCtrl");
   };
-})
 
-.controller('coolCtrl', function($scope){
-  $scope.whoAmI = function() {
-    console.log("this is the coolCtrl function!");
-  };
+  $scope.todos = [
+    {"name": "clean house"},
+    {"name": "think about John Stamos"},
+    {"name": "update beliefs"},
+    {"name": "begin to sense scents of handfulls of items, carefully"},
+    {"name": "be nice to wife"},
+    {"name": "clean up blood in the library"}
+  ]
 
-  // inheritance from the parent only takes place when there is not a value or method
-  // of the same name on the child.
-  // Prototypical inheritance flows only in one direction: from parent to children
-  // also: sibling controllers do not have access to each other's scopes
-  $scope.helloWorld = function() {
-    console.log("this is not the main ctrl!");
-  };
 });
