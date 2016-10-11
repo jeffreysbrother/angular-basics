869518c7957f9f7b886b368378837c0c58b4d4be
James Cool <jeffreysbrother@gmail.com>
2016-10-11 00:23:03 -0700

    inheritance

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,22 @@
 // the second argument is an array of dependencies
 angular.module("todoListApp", [])
+
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function() {
-    console.log("hello there! this is the helloWorld controller function in teh mainCtrl");
+    console.log("this is the helloWorld controller function in the mainCtrl");
+  };
+})
+
+.controller('coolCtrl', function($scope){
+  $scope.whoAmI = function() {
+    console.log("this is the coolCtrl function!");
+  };
+
+  // inheritance from the parent only takes place when there is not a value or method
+  // of the same name on the child.
+  // Prototypical inheritance flows only in one direction: from parent to children
+  // also: sibling controllers do not have access to each other's scopes
+  $scope.helloWorld = function() {
+    console.log("this is not the main ctrl!");
   };
 });
