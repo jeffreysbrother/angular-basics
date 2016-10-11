4275fe253fce572340deaa49c2b9cda6b575285d
James Cool <jeffreysbrother@gmail.com>
2016-10-10 23:57:56 -0700

    controller functioning

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,7 @@
 // the second argument is an array of dependencies
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log("hello there! this is the helloWorld controller function in teh mainCtrl");
+  };
+});
