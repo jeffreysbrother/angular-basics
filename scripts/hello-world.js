9986899cebb4c714ef89e3bd245942d2b69cca8e
James Cool <jeffreysbrother@gmail.com>
2016-10-10 23:46:25 -0700

    restrict

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,6 +1,8 @@
 angular.module('todoListApp')
 .directive('helloWorld', function() {
   return {
-    template: "This is the hello world directive"
+    template: "This is the hello world directive",
+    // restrict use as element only
+    restrict: "E"
   };
 });
